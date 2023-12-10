34825319c94a3270cffd93596a67a40bf441729f
daniloop404 <daniloop404@gmail.com>
2023-12-10 17:23:02 -0500

    Las facturas se muestran

--- screens/Perfil.js
+++ screens/Perfil.js
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet,ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getUsuarioPorId, putUsuario } from '../services/UsuariosService'; // Update the import path accordingly
 import { getUserKey, logout } from '../services/LoginService'; // Update the import path accordingly
-
+import { getFacturas } from '../services/FacturaService'; // Import the FacturaService
+import { FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import PDFView from 'react-native-view-pdf';
 const PerfilComponent = () => {
+  const [facturas, setFacturas] = useState([]);
+  const [selectedFactura, setSelectedFactura] = useState(null);
+
+  useEffect(() => {
+    const fetchFacturas = async () => {
+      const key = await getUserKey();
+      if (key) {
+        try {
+          const response = await getFacturas(key);
+          const facturasData = response.data || {};
+          const facturasArray = Object.values(facturasData); // Convert the object values to an array
+          setFacturas(facturasArray);
+        } catch (error) {
+          console.error('Error fetching facturas:', error);
+        }
+      }
+    };
+
+    fetchFacturas();
+  }, []);
   const [usuarioKey, setUsuarioKey] = useState(null);
   const [usuario, setUsuario] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
@@ -30,7 +53,6 @@ const PerfilComponent = () => {
 
     fetchUserData();
   }, []);
-
   const modificarDatos = () => {
     setIsEditing(true);
     // Create a copy of the user data for editing
@@ -67,8 +89,25 @@ const PerfilComponent = () => {
       console.error('Error during logout:', error);
     }
   };
+  const viewPDF = (factura) => {
+    setSelectedFactura(factura);
+  };
 
+  const closePDFViewer = () => {
+    setSelectedFactura(null);
+  };
+  const renderFacturaItem = ({ item }) => (
+    <View style={styles.facturaRow}>
+      <Text>{new Date(item.fecha).toLocaleDateString()}</Text>
+      <Text>${item.total.toFixed(2)}</Text>
+      <Text>{item.usuario.direccion}</Text>
+      <TouchableOpacity onPress={() => viewPDF(item)} style={styles.pdfIconLink}>
+        <Icon name="file-pdf-o" size={24} color="#fff" />
+      </TouchableOpacity>
+    </View>
+  );
   return (
+    <ScrollView>
     <View style={styles.container}>
       {usuario && (
         <View>
@@ -137,19 +176,54 @@ const PerfilComponent = () => {
                 </TouchableOpacity>
               </>
             )}
+            
           </View>
+
+          {/* Section to display PDF invoices */}
+          {selectedFactura && (
+          <View style={styles.pdfViewer}>
+            <TouchableOpacity onPress={closePDFViewer} style={styles.closeButton}>
+              <Text style={styles.buttonText}>Cerrar</Text>
+            </TouchableOpacity>
+            <PDFView
+              fadeInDuration={250.0}
+              
+              resource={selectedFactura.pdfUrl}
+              resourceType="url"
+              onLoad={() => console.log(`PDF rendered from ${selectedFactura.pdfUrl}`)}
+              onError={(error) => console.log('Cannot render PDF', error)}
+            />
+          </View>
+        )}
+
+<View style={styles.facturasContainer}>
+          <Text style={styles.headingFacturas}>Historial de Compras</Text>
+          <FlatList
+            data={facturas}
+            renderItem={renderFacturaItem}
+            keyExtractor={(item, index) => index.toString()}
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+          />
         </View>
-      )}
-    </View>
-  );
+      </View>
+    )}
+  </View>
+  </ScrollView>
+);
 };
 
 const styles = StyleSheet.create({
   container: {
+    
     marginVertical: 20,
     marginHorizontal: 10,
   },
   heading: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  headingFacturas: {
     fontSize: 20,
     textAlign: 'center',
     marginBottom: 10,
@@ -210,6 +284,58 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: '48%',
     alignItems: 'center',
+  },
+  pdfViewer: {
+    // position: 'absolute', // Remove this line
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: '#fff',
+    zIndex: 1000,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    padding: 10,
+    backgroundColor: 'black',
+    borderRadius: 5,
+    zIndex: 1100,
+  },
+  // Styles for Facturas
+  facturasContainer: {
+    marginTop: 20,
+    marginBottom: 20,
+    marginHorizontal: 10,
+  },
+  heading: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  facturasTable: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    paddingBottom: 10,
+  },
+  tableHeader: {
+    fontWeight: 'bold',
+  },
+  facturaRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center', // Add this line to center items vertically
+    marginVertical: 10,
+  },
+  pdfIconLink: {
+    backgroundColor: 'red',
+    padding: 5,
+    borderRadius: 5,
+  },
+  pdfIcon: {
+    color: 'white',
   },
 });
 
